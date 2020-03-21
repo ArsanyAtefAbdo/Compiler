2e9f32273386c55b22a2648489078c7e3f623c30
kirellos.malak <kiromalakhabib@gmail.com>
2020-03-21 22:41:10 +0200

    intial 4

---
+++ DFA/DFA.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kiro on 3/21/2020.
+//
+
+#include "DFA.h"
---
+++ DFA/DFA.h
@@ -0,0 +1,14 @@
+//
+// Created by Kiro on 3/21/2020.
+//
+
+#ifndef LEX_ANAL_GEN_DFA_H
+#define LEX_ANAL_GEN_DFA_H
+
+
+class DFA {
+
+};
+
+
+#endif //LEX_ANAL_GEN_DFA_H
