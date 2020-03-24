2c03df74665c90f0d80e6f3ca690a20ab24536cb
DELL <programmergood37@gmail.com>
2020-03-24 02:08:04 +0200

    Revert "solving some regex problems 05"
    This reverts commit 0300494d

--- Components/ReadLexicalRulesFile.h
+++ Components/ReadLexicalRulesFile.h
@@ -21,10 +21,10 @@ private:
     vector <pair<string,string>> regular_expression_vector;
     vector <string> keywords_vector;
     vector <string> punctuations_vector;
-    std::regex regular_definition_regex;
-    std::regex regular_expression_regex;
-    std::regex keywords_regex;
-    std::regex punctuations_regex;
+    regex regular_definition_regex;
+    regex regular_expression_regex;
+    regex keywords_regex;
+    regex punctuations_regex;
     smatch match;
     bool no_errors = true;
 
