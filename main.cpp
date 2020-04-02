a4cdc70c4280074b7a1ac8befafa7d0c4e9c4b27
DELL <programmergood37@gmail.com>
2020-04-02 19:05:47 +0200

    Final commit

--- main.cpp
+++ main.cpp
@@ -14,36 +14,20 @@ int main() {
     map<string, int> priorities{};
     vector<LexicalRule*>rules = ReadLexicalRulesFile::getInstance()->read_from_file("test.txt", &priorities);
     NFA* nfa = Builder::getInstance()->buildNFAFromLexicalRules(rules, priorities);
-//    for(Node* node: nfa->getFinalStates()){
-//        cout << node->getName() << " " << node->isFinalState()<<endl;
-//    }
 
-    //------------------------------
+    //--------------convert NFA to DFA----------------
 
     DFA* dfa = Converter::getInstance()->convert(nfa, Builder::getInstance()->getAlphabet());
     map<Node *, map<char, Node *>> ans = dfa->getDTable();
 
-//    for(pair<Node *, map<char,  Node *>>row : ans){
-//        cout << row.first->getName() << " : ";
-//        for(pair<char, Node *> n :row.second){
-//            cout << "(" << n.first << " , " << n.second->getName() << ")  ";
-//        }
-//        cout << endl;
-//    }
-
+    //--------------Minimize the DFA Table and save it----------------
     Minimizer::getInstance()->DFAMinimize(dfa);
     ans = dfa->getDTable();
 
     ReadLexicalRulesFile::getInstance()->printTable("table", ans, Builder::getInstance()->getAlphabet());
 
-//    cout << "---------after minimizing ----------------"<< endl;
-//    for(pair<Node *, map<char,  Node *>>row : ans){
-//        cout << row.first->getName() << " : " << row.first->isFinalState();
-//        for(pair<char, Node *> n :row.second){
-//            cout << "(" << n.first << " , " << n.second->getName() << ")  ";
-//        }
-//        cout << endl;
-//    }
+
+    //----------------Scanning the input program--------------
     vector<pair<string, string>> tokens = Scanner::getInstance()->scanProgramFile("prog.txt",  dfa);
     for(const pair<string, string>&token : tokens){
         cout << token.second << endl;
